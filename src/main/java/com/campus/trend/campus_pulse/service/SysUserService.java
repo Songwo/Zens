f2611317216be2c2f00076aae63c96073e7d9be7
SongWo <zhaoqsnyah@gmail.com>
2025-11-25 15:28:54 +0800

    新增Service包 1.创建SysUserService接口，继承了IService<SysUser> 2.创建SysUserServiceImpl类，继承了Mybatis的ServiceImpl<SysUserMapper,SysUser>,实现了SysUserService 3.行程业务层闭环

---
+++ src/main/java/com/campus/trend/campus_pulse/service/SysUserService.java
@@ -0,0 +1,7 @@
+package com.campus.trend.campus_pulse.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.campus.trend.campus_pulse.entity.SysUser;
+
+public interface SysUserService extends IService<SysUser> {
+}
---
+++ src/main/java/com/campus/trend/campus_pulse/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.campus.trend.campus_pulse.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.campus.trend.campus_pulse.entity.SysUser;
+import com.campus.trend.campus_pulse.mapper.SysUserMapper;
+import com.campus.trend.campus_pulse.service.SysUserService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+
+}
+
